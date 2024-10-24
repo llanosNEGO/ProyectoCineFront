b4eaa1c07c7e809bbdea3069ea454258d26fc014
CernaAlt <152317741+CernaAlt@users.noreply.github.com>
2024-10-24 18:59:04 -0500

    Cambios 02

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ng-cinerama-18',
+        appId: '1:891794302026:web:2745a8bf4ba7201b2272b3',
+        storageBucket: 'ng-cinerama-18.appspot.com',
+        apiKey: 'AIzaSyDtmxTzo1_yasKPEn6NxEikLbOkXeiTe8I',
+        authDomain: 'ng-cinerama-18.firebaseapp.com',
+        messagingSenderId: '891794302026',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
